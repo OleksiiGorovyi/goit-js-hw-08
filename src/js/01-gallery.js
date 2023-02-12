3aa4d84fd9308ac414f3a31eabb88d6947826f14
OleksiiGorovyi <alekseygorovoyyyy@gmail.com>
2023-02-12 11:47:13 +0200

    hw8 more changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryImages = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+    return items
+        .map(
+            item =>
+                 `<li><a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+          </li></a>`
+    )
+    .join('');
+}
+
+gallaryImages.style.listStyle = 'none';
+
+const imgMarkup = createGalleryMarkup(galleryItems);
+
+galleryImages.innerHTML = imgMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 300 });
